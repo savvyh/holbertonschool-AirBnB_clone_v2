49c3695fdabb4627c9169151ccce2f537e7d3e58
savvyh <7277@holbertonstudents.com>
2024-04-09 21:54:31 +0000

    Add a route for the function that handle the template number

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    new_text = text.replace('_', ' ')
+    return 'C ' + new_text
+
+
+@app.route('/python', defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    new_text = text.replace('_', ' ')
+    return 'Python' + new_text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def n_is_integer(n):
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/int:<n>', strict_slashes=False)
+def n_template(n):
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
