84146cea29a9943f1028a943c0fc918c9b3d4e61
savvyh <7277@holbertonstudents.com>
2024-04-09 21:07:24 +0000

    update with my personal code the file hello-route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,13 +4,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# Ensures strict_slashes are set to False for all routes globally.
-app.url_map.strict_slashes = False
 
-
-@app.route("/")
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-    return "<p>Hello HBNB!</p>"
+    return "Hello HBNB!"
 
 
 if __name__ == '__main__':
