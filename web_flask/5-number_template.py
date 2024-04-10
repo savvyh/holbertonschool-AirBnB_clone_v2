197bf3809df3dec61fcb7a63e3f7ebfa1d9554dc
savvyh <7277@holbertonstudents.com>
2024-04-10 08:32:38 +0000

    Adding a render_template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+"""
+Functions:
+    app.route '/'
+    app.route '/hbnb'
+    app.route '/c/<text>'
+    app.route '/python/<text>'
+    app.route '/python'
+    app.route '/number/<int:n>'
+    app.route '/number_template/int:<n>'
+Returns:
+    text
+"""
 
 from flask import Flask, render_template
 
@@ -7,16 +19,19 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Display 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Display 'HBNB'"""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
+    """Display 'C + text replace'"""
     new_text = text.replace('_', ' ')
     return 'C ' + new_text
 
@@ -24,17 +39,20 @@ def c_text(text):
 @app.route('/python', defaults={'text': 'is_cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text='is cool'):
+    """Display 'Python + text repplace or Python + 'is cool'"""
     new_text = text.replace('_', ' ')
-    return 'Python' + new_text
+    return 'Python ' + new_text
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def n_is_integer(n):
+    """Display 'n is a number'"""
     return '{} is a number'.format(n)
 
 
 @app.route('/number_template/int:<n>', strict_slashes=False)
 def n_template(n):
+    """Display render_template"""
     return render_template('5-number.html', n=n)
 
 
