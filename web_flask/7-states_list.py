fdc7b743d81c7935ce7fd19ee48a75f1553c0af4
savvyh <7277@holbertonstudents.com>
2024-04-10 10:06:11 +0000

    Adding a route to sorted in alphabetical order all the states in storage

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Functions:
+    app.route '/states_list'
+Returns:
+    text
+"""
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display a sorted list in alphabetical order with all states"""
+    states = storage.all('States').values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Closes the storage at the end of the request."""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
