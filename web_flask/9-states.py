a20e6b5780c5ebf129e391dcd47048bbeb8706af
savvyh <7277@holbertonstudents.com>
2024-04-11 11:16:29 +0000

    Split the 2 app route for the state

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -15,13 +15,22 @@ app = Flask(__name__)
 
 
 @app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
-def states(state_id=None):
+def list_states():
     """display the states and cities listed in alphabetical order"""
     states = storage.all(State).values()
-    if state_id is not None:
-        state_id = 'State.' + state_id
-    return render_template('9-states.html', states=states, state_id=state_id)
+    return render_template('9-states.html', states=states, id=None)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def list_states_id(id):
+    """display the states and cities listed in alphabetical order, with id"""
+    states = storage.all(State).values()
+    state = None
+    for s in states:
+        if s.id == id:
+            state = s
+            break
+    return render_template('9-states.html', state=state)
 
 
 @app.teardown_appcontext
